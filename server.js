56dee867939826f5fb1b715dbc04f1ffdb2197aa
Emre Aras <33624409+arasmr@users.noreply.github.com>
2025-03-02 02:23:15 +0100

    add authentication

--- server.js
+++ server.js
@@ -1,75 +1,39 @@
 const express = require("express");
 const axios = require("axios");
-const CircularJSON = require("circular-json");
-const https = require("https");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000; // You can change the port as needed
 
 app.use(express.json()); // To parse incoming JSON requests
 
-const agent = new https.Agent({
-  rejectUnauthorized: false, // Disables hostname verification
-});
+// Replace with your actual API key and secret
+const VALID_API_KEY = process.env.API_KEY;
+const VALID_API_SECRET = process.env.API_SECRET;
 
-// Proxy route that forwards the incoming request
-// app.use("/", async (req, res) => {
-//   console.log("emre", req.headers);
-//   try {
-//     const targetUrl = "https://partscentraalws.aldoc.eu:443" + req.originalUrl; // Change to your target app URL
+// Middleware to verify API key and secret
+const verifyApiKeyAndSecret = (req, res, next) => {
+  const apiKey = req.headers["x-api-key"];
+  const apiSecret = req.headers["x-api-secret"];
 
-//     console.log("Forwarded request headers:", {
-//       ...req.headers,
-//       "User-Agent": "curl/8.5.0",
-//       Accept: "application/json",
-//     });
+  if (!VALID_API_KEY || !VALID_API_SECRET) {
+    // Invalid API key or secret
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Invalid API key or secret" });
+  }
 
-//     console.log("Request body:", req.body);
-//     console.log("Request query:", req.query);
+  if (apiKey === VALID_API_KEY && apiSecret === VALID_API_SECRET) {
+    // API key and secret are valid, proceed to the next middleware/route
+    return next();
+  }
 
-//     console.log("Target URL:", targetUrl);
+  // Invalid API key or secret
+  res.status(401).json({ message: "Unauthorized: Invalid API key or secret" });
+};
 
-//     // Forward the request using axios
-//     const axiosConfig = {
-//       method: req.method,
-//       url: targetUrl,
-//       headers: {
-//         ...req.headers,
-//         "User-Agent": "curl/8.5.0",
-//         Accept: "application/json",
-//       },
-//       httpsAgent: agent,
-//       data: req.body, // Forward body if there's one
-//       params: req.query, // Forward query params if there's any
-//     };
-
-//     // Log the config being used for the request
-//     console.log("Axios config:", axiosConfig);
-
-//     const response = await axios(axiosConfig);
-
-//     console.log("Response status:", response.status);
-
-//     console.log("Full response:", response);
-
-//     // console.log("response.data emre", CircularJSON.stringify(response));
-//     console.log("response.data emre", CircularJSON.stringify(response.data));
-
-//     // Send back the response from the target application
-//     res.status(response.status).send(response.data);
-//   } catch (error) {
-//     if (error.response) {
-//       console.log("Error response:", error.response);
-//     }
-//     console.log("error in catch block", error);
-//     // Handle errors and send an appropriate response
-//     if (error.response) {
-//       res.status(error.response.status).send(error.response.data);
-//     } else {
-//       res.status(500).send("Error occurred while forwarding the request");
-//     }
-//   }
-// });
+// Apply the verification middleware to all routes
+app.use(verifyApiKeyAndSecret);
 
 app.use("/", async (req, res) => {
   const targetUrl = "https://partscentraalws.aldoc.eu:443" + req.originalUrl; // Change to your target app URL
@@ -84,7 +48,6 @@ app.use("/", async (req, res) => {
       },
     })
     .then((response) => {
-      console.log(response.data);
       res.status(response.status).send(response.data);
     })
     .catch((error) => {
